b07e218a36d62d2d318ada4acb2ea762796a1bd7
Pranay Maharana <pranay.maharana@gmail.com>
2017-04-16 22:15:12 -0400

    --testing rental log

--- Blockbuster/Models/RentalLog.cs
+++ Blockbuster/Models/RentalLog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,13 +8,26 @@ namespace Blockbuster.Models
 {
     public class RentalLog
     {
-        public int Id { get; set; }
-        public int CustomerId { get; set; }
-        public int MovieId { get; set; }
+        public int? Id { get; set; }
+        public int? CustomerId { get; set; }
+        public int? MovieId { get; set; }
         public DateTime? DateCheckedOut { get; set; }
-        public DateTime? DueDate { get; set; }
+        public string DueDate { get; set; }
         public DateTime? ReturnDate { get; set; }
         public Customers Customer { get; set; }
         public Movies Movie { get; set; }
+
+        //public RentalLog(SqlDataReader reader)
+        //{
+        //    this.Id = (int)reader["Id"];
+        //    this.CustomerId = (int)reader["CustomerId"];
+        //    this.MovieId = (int)reader["MovieId"];
+        //    this.DateCheckedOut = (DateTime)reader["DateCheckedOut"];
+
+        //    //using .Date poperty to set DateAdded
+        //    var dueBackDate = (DateTime)reader["DueBackDate"];
+        //    var dateString = dueBackDate.AddDays(10).ToShortDateString();
+        //    this.DueDate = dateString;
+        //}
     }
 }
